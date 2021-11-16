61b17380eba9d28059230011be7d6d2a61990be8
jinicha <jinichaa@gmail.com>
2021-11-15 18:14:41 -0800

    Complete Step Three

--- stock-news-hard-start/main.py
+++ stock-news-hard-start/main.py
@@ -1,4 +1,6 @@
 import requests
+from twilio.rest import Client
+from lxml import html
 import config
 
 STOCK = "TSLA"
@@ -22,32 +24,40 @@ stock_data = stock_response.json()["Time Series (Daily)"]
 latest_two_days = list(stock_data.items())[:2]
 latest_adjusted_close_data = float(latest_two_days[0][1]["5. adjusted close"])
 the_day_before_latest_adjusted_close_data = float(latest_two_days[1][1]["5. adjusted close"])
-absolute_diff = round(abs(latest_adjusted_close_data - the_day_before_latest_adjusted_close_data), 2)
+diff = round(latest_adjusted_close_data - the_day_before_latest_adjusted_close_data, 2)
+absolute_diff = abs(diff)
 diff_rate = round((absolute_diff / latest_adjusted_close_data), 2)
+
+
+def send_alert(news_list):
+    account_sid = config.account_sid
+    auth_token = config.auth_token
+    client = Client(account_sid, auth_token)
+    for news in news_list:
+        title = html.fromstring(news["title"]).text_content()
+        brief = html.fromstring(news["description"]).text_content()
+        if diff >= 0:
+            message = client.messages.create(
+                body=f'{STOCK}: 🔺{diff_rate * 100}%\n\nHeadline: {title}\n\nBrief:{brief}',
+                from_=config.send_from,
+                to=config.send_to
+            )
+        else:
+            message = client.messages.create(
+                body=f'{STOCK}: 🔻{diff_rate * 100}%\n\nHeadline: {title}\n\nBrief: {brief}',
+                from_=config.send_from,
+                to=config.send_to
+            )
+
+    print(message.status)
+
 
 if diff_rate >= 0.05:
     news_response = requests.get(url=NEWS_ENDPOINT, params=news_params)
     news_data = news_response.json()["articles"][:3]
-    print(news_data)
-
-
-# STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-# HINT 1: Consider using a List Comprehension.
-
-
-# Optional: Format the SMS message like this:
-# """
-# TSLA: 🔺2%
-# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
-# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to
-# file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height
-# of the coronavirus market crash.
-# or
-# "TSLA: 🔻5%
-# Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
-# Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to
-# file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height
-# of the coronavirus market crash.
-# """
-
+    send_alert(news_data)
+else:
+    if diff >= 0:
+        print('Increased less than 5%')
+    else:
+        print('Decreased less than 5%')
