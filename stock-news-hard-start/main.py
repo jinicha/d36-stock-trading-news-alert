4777a9022331075c5f6ecdb3b0de21a1323e50e3
jinicha <jinichaa@gmail.com>
2021-11-15 19:05:19 -0800

    Clean up

--- stock-news-hard-start/main.py
+++ stock-news-hard-start/main.py
@@ -21,38 +21,39 @@ news_params = {
 
 stock_response = requests.get(url=STOCK_ENDPOINT, params=stock_params)
 stock_data = stock_response.json()["Time Series (Daily)"]
+
 latest_two_days = list(stock_data.items())[:2]
-latest_adjusted_close_data = float(latest_two_days[0][1]["5. adjusted close"])
-the_day_before_latest_adjusted_close_data = float(latest_two_days[1][1]["5. adjusted close"])
-diff = round(latest_adjusted_close_data - the_day_before_latest_adjusted_close_data, 2)
-absolute_diff = abs(diff)
-diff_rate = round((absolute_diff / latest_adjusted_close_data), 2)
+latest_adjusted_closing_price = float(latest_two_days[0][1]["5. adjusted close"])
+the_day_before_latest_adjusted_closing_price = float(latest_two_days[1][1]["5. adjusted close"])
+diff = latest_adjusted_closing_price - the_day_before_latest_adjusted_closing_price
+diff_rate = round((abs(diff) / latest_adjusted_closing_price), 2)
+sign = None
+
+if diff > 0:
+    sign = "🔺"
+elif diff < 0:
+    sign = "🔻"
 
 
 def send_alert(news_list):
     account_sid = config.account_sid
     auth_token = config.auth_token
     client = Client(account_sid, auth_token)
+
     for news in news_list:
         title = html.fromstring(news["title"]).text_content()
         brief = html.fromstring(news["description"]).text_content()
-        if diff >= 0:
-            message = client.messages.create(
-                body=f'{STOCK}: 🔺{diff_rate * 100}%\n\nHeadline: {title}\n\nBrief:{brief}',
-                from_=config.send_from,
-                to=config.send_to
-            )
-        else:
-            message = client.messages.create(
-                body=f'{STOCK}: 🔻{diff_rate * 100}%\n\nHeadline: {title}\n\nBrief: {brief}',
-                from_=config.send_from,
-                to=config.send_to
-            )
+
+        message = client.messages.create(
+            body=f'{STOCK}: {sign}{diff_rate * 100}%\n\nHeadline: {title}\n\nBrief: {brief}',
+            from_=config.send_from,
+            to=config.send_to
+        )
 
     print(message.status)
 
 
-if diff_rate >= 0.05:
+if diff_rate > 0.05:
     news_response = requests.get(url=NEWS_ENDPOINT, params=news_params)
     news_data = news_response.json()["articles"][:3]
     send_alert(news_data)
